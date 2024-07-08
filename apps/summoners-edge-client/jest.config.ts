0d0431ecae54ad07caa25b26b94394cbbe340eb2
Patrik Larsson <patrik.larssson@outlook.com>
2024-07-08 14:22:44 +0200

    electron + next 14

---
+++ apps/summoners-edge-client/jest.config.ts
@@ -0,0 +1,11 @@
+/* eslint-disable */
+export default {
+  displayName: 'summoners-edge-client',
+  preset: '../../jest.preset.js',
+  testEnvironment: 'node',
+  transform: {
+    '^.+\\.[tj]s$': ['ts-jest', { tsconfig: '<rootDir>/tsconfig.spec.json' }],
+  },
+  moduleFileExtensions: ['ts', 'js', 'html'],
+  coverageDirectory: '../../coverage/apps/summoners-edge-client',
+};
---
+++ apps/summoners-edge-client/src/app/constants.ts
@@ -0,0 +1,4 @@
+export const rendererAppPort = 3000;
+export const rendererAppName = 'summoners-edge'; // options.name.split('-')[0] + '-web'
+export const electronAppName = 'summoners-edge-client';
+export const updateServerUrl = 'https://deployment-server-url.com'; // TODO: insert your update server url here
