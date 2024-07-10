dca897c5fe89f9ba4e7ade0a27b74384d0325498
Patrik Larsson <patrik.larssson@outlook.com>
2024-07-10 15:50:45 +0200

    test data

--- apps/summoners-edge/src/app/champions/[champion]/page.tsx
+++ apps/summoners-edge/src/app/champions/[champion]/page.tsx
@@ -38,16 +38,27 @@ const ChampionPage = async ({ params }: ChampionPageProps) => {
   const champUrl = `http://localhost:3200/champion/${params.champion}`;
   const champion = (await axios.get<ChampionStatsDto>(champUrl)).data;
 
+  const dataUrl = `http://localhost:3200/cdn/14.13.1/data/en_US/champion/${params.champion}.json`;
+  const data = (await axios.get(dataUrl)).data;
+
+  const spells = data.data[params.champion].spells
+  
+
   return (
     <div>
-      <img src={`${champUrl}.png`} />
+      <img src={`http://localhost:3200/cdn/14.13.1/img/champion/${params.champion}.png`} />
       <h1>{champion.championName}</h1>
+      <div>
+        {spells.map((x) => (
+          <img src={`http://localhost:3200/cdn/14.13.1/img/spell/${x.image.full}`} />
+        ))}
+      </div>
       <div>
         {champion.items.map((x) => (
           <img
             width={80}
             height={80}
-            src={`http://localhost:3200/item/${x.itemId}.png`}
+            src={`http://localhost:3200/cdn/14.13.1/img/item/${x.itemId}.png`}
           />
         ))}
       </div>
